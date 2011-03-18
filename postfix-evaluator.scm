d36339d461c972de6484f3d7812564673486cb51
Andrew Levenson <andrew.levenson@gmail.com>
2011-03-18 11:38:19 -0400

    Added rudimentary definitions of pi and cosine.

--- postfix-evaluator.scm
+++ postfix-evaluator.scm
@@ -7,6 +7,8 @@
       1
       (* n (factorial (- n 1)))))
 
+(define pi 3.14159265)
+
 (define (sine x . n)
   (cond ((not (null? n))
          (cond ((< 25 (car n))
@@ -14,8 +16,11 @@
                (else (- (/ (expt x (car n)) (factorial (car n)))
                         (sine x (+ 2 (car n)))))))
         (else (- x (sine x 3)))))
-  
-  
+
+(define (cosine x)
+  (if (= x 1)
+      0
+      (sine (- (/ pi 2) x))))
    
 (define (char->operator char)
   (cond ((eq? char #\+) '+)
